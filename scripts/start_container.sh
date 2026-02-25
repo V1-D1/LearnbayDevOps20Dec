8f3391313a470a4bd9efee9ec8eeb5d1cc62b465
Vishal Dhilpe <vdhilpe@gmail.com>
2026-02-25 16:29:11 +0530

    update scripts

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,13 +5,13 @@ REGION="ap-south-1"
 
 # Get credentials from SSM
 DOCKER_USERNAME=$(aws ssm get-parameter \
-  --name "/learnbayapp/dockercredentials/username1" \
+  --name "/learnbayapp/dockercredentials/username" \
   --region $REGION \
   --query "Parameter.Value" \
   --output text)
 
 DOCKER_PASSWORD=$(aws ssm get-parameter \
-  --name "/learnbayapp/dockercredentials/password1" \
+  --name "/learnbayapp/dockercredentials/password" \
   --with-decryption \
   --region $REGION \
   --query "Parameter.Value" \
